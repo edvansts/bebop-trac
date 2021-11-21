e2d89ff8ca1b8cb6c1ef320b306e859d11d8ece3
Edvan de Matos <edvan.stt02@gmail.com>
2021-11-20 22:47:08 -0300

    feat: :sparkles: header created

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,17 +1,39 @@
-import { Image, Layout } from "antd";
+import { Image, Layout, Button } from "antd";
 
 import logo from "../../assets/logo.png";
 
+import styles from "./Topbar.module.scss";
+
 function Topbar() {
   return (
-    <Layout.Header style={{ padding: '0.625rem 1.25rem' }}>
-      <Image
-        src={logo}
-        alt="logo"
-        preview={false}
-        width="calc(height * 2.5)"
-        height="100%"
-      />
+    <Layout.Header>
+      <div className={styles.innerHeader}>
+        <Image
+          src={logo}
+          alt="logo"
+          preview={false}
+          className={styles.logo}
+          height="100%"
+        />
+
+        <ul className={styles.navigation}>
+          <Button type="link" href="/">
+            <li>Home</li>
+          </Button>
+          <Button type="link" href="/ativos">
+            <li>Ativos</li>
+          </Button>
+          <Button type="link" href="/empresas">
+            <li>Empresas</li>
+          </Button>
+          <Button type="link" href="/unidades">
+            <li>Unidades</li>
+          </Button>
+          <Button type="link" href="/usuarios">
+            <li>Usuários</li>
+          </Button>
+        </ul>
+      </div>
     </Layout.Header>
   );
 }
